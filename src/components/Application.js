9573e30ed2894288e3b69df15365dbbed51362ea
Jordan <jordan.benson4@gmail.com>
2022-02-25 09:36:46 -0800

    Refactor to use Promise.all for multiple axios get requests.

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,66 +3,69 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import axios from "axios";
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
+import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application(props) {
-const [state, setState] = useState({
-  day: "Monday",
-  days: [],
-  appointments: []
-});
-const setDay = day => setState({...state, day})
-const setDays = (days) => {
-  setState(prev => ({...prev, days}));
-}
-const appointmentArray = appointments.map((appointment) =>
-<Appointment
-  key={appointment.id}
-  {...appointment}
-/>
-)
-  useEffect(() => {
-    axios.get('/api/days')
-      .then(response => setDays(response.data))
-  }, [])
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: [
+      {
+        id: 1,
+        time: "12pm",
+      },
+      {
+        id: 2,
+        time: "1pm",
+        interview: {
+          student: "Lydia Miller-Jones",
+          interviewer:{
+            id: 3,
+            name: "Sylvia Palmer",
+            avatar: "https://i.imgur.com/LpaY82x.png",
+          }
+        }
+      },
+      {
+        id: 3,
+        time: "2pm",
+      },
+      {
+        id: 4,
+        time: "3pm",
+        interview: {
+          student: "Archie Andrews",
+          interviewer:{
+            id: 4,
+            name: "Cohana Roy",
+            avatar: "https://i.imgur.com/FK8V841.jpg",
+          }
+        }
+      },
+      {
+        id: 5,
+        time: "4pm",
+      }
+    ]
+  });
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const setDay = day => setState({...state, day})
+
+  const appointmentArray = dailyAppointments.map((appointment) =>
+  <Appointment
+    key={appointment.id}
+    {...appointment}
+  />
+  )
+  Promise.all([
+    axios.get('/api/days'),
+    axios.get('/api/appointments'),
+    axios.get('/api/interviewers'),
+  ]).then((all) => {
+    const [ days, appointments, interviewers ] = all;
+    setState(prev => ({...prev, days: days.data, appointments: appointments.data}))
+  })
+
   return (
     <main className="layout">
       <section className="sidebar">
