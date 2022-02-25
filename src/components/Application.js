58be0aa009a5d5f520ad35cb0f05f9c909c6d0e5
Jordan <jordan.benson4@gmail.com>
2022-02-24 17:17:47 -0800

    Remove hardcoded days list and use axios to get days data.

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
+import axios from "axios";
 
 const appointments = [
   {
@@ -42,33 +43,19 @@ const appointments = [
   }
 ];
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
   const appointmentArray = appointments.map((appointment) =>
     <Appointment
       key={appointment.id}
       {...appointment}
     />
   )
-  console.log(appointmentArray);
+  useEffect(() => {
+    axios.get('/api/days')
+      .then(response => setDays(response.data))
+  })
   return (
     <main className="layout">
       <section className="sidebar">
