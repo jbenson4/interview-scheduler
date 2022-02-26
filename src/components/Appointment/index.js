e208ae3f355964cac81631122c064601e11bba11
Jordan <jordan.benson4@gmail.com>
2022-02-26 11:39:50 -0800

    Add interviewers props to Form component.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -19,7 +19,7 @@ export default function Appointment(props) {
       <Header time={props.time}/>
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && <Show student={props.interview.student} interviewer={props.interview.interviewer} />}
-      {mode === CREATE && <Form onCancel={() => back()} interviewers={[]} />}
+      {mode === CREATE && <Form onCancel={() => back()} interviewers={props.interviewers} />}
     </article>
   );
 }
