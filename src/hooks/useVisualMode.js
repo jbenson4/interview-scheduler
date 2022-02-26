46e9abf87e25573bc58074795d3b7d175ab4211c
Jordan <jordan.benson4@gmail.com>
2022-02-26 10:16:24 -0800

    Fix back function if statement and clarify mode variable name.

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,20 +3,22 @@ import { useState } from "react";
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
-  const transition = function (mode, replace = false) {
+
+  const transition = function (newMode, replace = false) {
     if (replace) {
-      setMode(mode);
+      setMode(newMode);
       const newHistory = [...history];
-      newHistory[newHistory.length - 1] = mode;
+      newHistory[newHistory.length - 1] = newMode;
       setHistory(newHistory);      
     }
-    history.push(mode);
-    setMode(mode);
+    history.push(newMode);
+    setMode(newMode);
   };
   const back = function () {
-    if (history.length > 1)
-    history.pop()
-    setMode(history[history.length - 1]);
+    if (history.length > 1) {
+      history.pop()
+      setMode(history[history.length - 1]);
+    }
   };
   return { mode, transition, back };
 };
