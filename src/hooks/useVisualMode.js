fa0c290cb1ada6e2c1c9a3dd1b0f57033a5f4957
Jordan <jordan.benson4@gmail.com>
2022-02-25 21:58:30 -0800

    Add seperate functions to custom hook.

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,6 +2,21 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
-
-  return {mode};
-}
+  const [history, setHistory] = useState([initial]);
+  const transition = function (mode, replace = false) {
+    if (replace) {
+      setMode(mode);
+      const newHistory = [...history];
+      newHistory[newHistory.length - 1] = mode;
+      setHistory(newHistory);      
+    }
+    history.push(mode);
+    setMode(mode);
+  };
+  const back = function () {
+    if (history.length > 1)
+    history.pop()
+    setMode(history[history.length - 1]);
+  };
+  return { mode, transition, back };
+};
