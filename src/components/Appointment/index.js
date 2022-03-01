e5b4350c4f751cf704435cd2cf5fba42b84b2ac8
Jordan <jordan.benson4@gmail.com>
2022-02-28 18:54:23 -0800

    Create save function and add STATUS mode for saving.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,6 +5,7 @@ import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
 import useVisualMode from "hooks/useVisualMode";
+import Status from "./Status";
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
@@ -13,13 +14,27 @@ export default function Appointment(props) {
   const CONFIRM = "CONFIRM";
   const ERROR = "ERROR";
   const FORM = "FORM";
+  const STATUS ="STATUS";
   const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
+
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    transition(STATUS);
+    props.bookInterview(props.id, interview)
+      .then(() => transition(SHOW))
+  }
+
+
   return (
     <article className="appointment">
       <Header time={props.time}/>
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && <Show student={props.interview.student} interviewer={props.interview.interviewer} />}
-      {mode === CREATE && <Form onCancel={() => back()} interviewers={props.interviewers} />}
+      {mode === CREATE && <Form onCancel={() => back()} interviewers={props.interviewers} onSave={save}/>}
+      {mode === STATUS && <Status message={"SAVING"}/>}
     </article>
   );
 }
