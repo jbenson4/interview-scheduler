ad7bd391fd7b8662f56f47142c76c7e3e50dde7f
Jordan <jordan.benson4@gmail.com>
2022-03-03 10:01:37 -0800

    Add interviewer validation to validate function.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -14,22 +14,25 @@ export default function Form(props) {
   const cancel = () => {
     reset();
     props.onCancel();
-  }; 
+  };
 
   const validate = () => {
     if (student === "") {
       setError("Student name cannot be blank");
       return;
     }
-    setError("")
+    if (interviewer === null) {
+      setError("Interviewer must be chosen");
+      return;
+    }
+    setError("");
     props.onSave(student, interviewer);
-  }
+  };
 
-  
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
           <input
             data-testid="student-name-input"
             className="appointment__create-input text--semi-bold"
@@ -37,11 +40,11 @@ export default function Form(props) {
             type="text"
             value={student}
             placeholder="Enter Student Name"
-            onChange={(event) => setStudent(event.target.value) }
+            onChange={(event) => setStudent(event.target.value)}
           />
         </form>
         <section className="appointment__validation">{error}</section>
-        <InterviewerList 
+        <InterviewerList
           interviewers={props.interviewers}
           value={interviewer}
           onChange={setInterviewer}
@@ -49,8 +52,12 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={validate}>Save</Button>
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={validate}>
+            Save
+          </Button>
         </section>
       </section>
     </main>
